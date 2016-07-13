c7c86bc0e34d54ee36ffc4360c094ebf2f030436
sthuang <sthuang@dl-tek.com>
2016-07-13 09:22:37 +0800

    重構細分

--- ClassLibrary1/Book_CalculatorPrice.cs
+++ ClassLibrary1/Book_CalculatorPrice.cs
@@ -16,39 +16,60 @@ namespace ClassLibrary1
         {
             while (books.Any())
             {
-                var bookTypeCount = books.Select(x => x.Episode).Distinct().Count();
-                switch (bookTypeCount)
-                {
-                    case 2:
-                        discount = 0.95;
-                        break;
-                    case 3:
-                        discount = 0.9;
-                        break;
-                    case 4:
-                        discount = 0.8;
-                        break;
-                    case 5:
-                        discount = 0.75;
-                        break;
-                    default:
-                        discount = 1;
-                        break;
-                }            
+                var bookTypeCount = GetBookDiscount(books);  
+                var PreferentialPooterBooks = GetPreferentialPooterBooks(books);
 
-                List<Book> preferentialBooks = new List<Book>();
-                foreach (var Episode in books.Select(x => x.Episode).Distinct().ToList())
-                {
-                    preferentialBooks.Add(books.Find(x => x.Episode == Episode));
-                }
-
-                foreach (var book in preferentialBooks)
-                {
-                    total += (book.Price * discount);
-                    books.Remove(book);
-                }              
+                GetBookDisCountPrice(books, PreferentialPooterBooks, discount);
+                
             }
             return total;
+        }
+
+
+        //取得折扣%數
+        private double GetBookDiscount(List<Book> books)
+        {
+            var bookTypeCount = books.Select(x => x.Episode).Distinct().Count();
+            switch (bookTypeCount)
+            {
+                case 2:
+                    discount = 0.95;
+                    break;
+                case 3:
+                    discount = 0.9;
+                    break;
+                case 4:
+                    discount = 0.8;
+                    break;
+                case 5:
+                    discount = 0.75;
+                    break;
+                default:
+                    discount = 1;
+                    break;
+            }
+            return discount;
+        }
+
+        //計算
+        private void GetBookDisCountPrice(List<Book> books, List<Book> preferentialBooks, double disCount)
+        {
+            foreach (var book in preferentialBooks)
+            {
+                total += (book.Price * discount);
+                books.Remove(book);
+            }
+        }
+
+        //書本優惠是否達到優惠折扣,未達到以原價計算      
+        private List<Book> GetPreferentialPooterBooks(List<Book> books)
+        {
+            List<Book> preferentialBooks = new List<Book>();
+            foreach (var Episode in books.Select(x => x.Episode).Distinct().ToList())
+            {
+                preferentialBooks.Add(books.Find(x => x.Episode == Episode));
+            }
+            return preferentialBooks;
         }
     } 
 }
